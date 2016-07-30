58fcc16034cbc3b0a5e6a27229bb014eb259ebc9
EVMosaic <evarela@mosaicLearning.com>
2016-07-30 15:09:45 -0400

    initial commit

---
+++ Assets/Editor/HexCoordinateDrawer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(HexCoordinates))]
+public class HexCoordinatesDrawer : PropertyDrawer
+{
+  public override void OnGUI(
+    Rect position, SerializedProperty property, GUIContent label
+  )
+  {
+    position = EditorGUI.PrefixLabel(position, label);
+
+    HexCoordinates coordinates = new HexCoordinates(
+      property.FindPropertyRelative("x").intValue,
+      property.FindPropertyRelative("z").intValue
+    );
+
+    GUI.Label(position, coordinates.ToString());
+  }
+
+}
---
+++ Assets/HexCell.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexCell : MonoBehaviour {
+  public HexCoordinates coordinates;
+
+  public Color color;
+
+  [SerializeField]
+  HexCell[] neighbors;
+
+  public HexCell GetNeighbor (HexDirection direction, HexCell cell)
+  {
+    return neighbors[(int)direction];
+  }
+
+  public void SetNeighbor (HexDirection direction, HexCell cell)
+  {
+    neighbors[(int)direction] = cell;
+    cell.neighbors[(int)direction.Opposite()] = this;
+
+  }
+
+
+
+}
